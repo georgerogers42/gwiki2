c8723f10b750b87efd9ad9a3c69a3df21d6a3632
George Rogers <george@localhost.localdomain>
2012-01-09 17:22:28 -0600

    deploy

--- hello/hello.go
+++ hello/hello.go
@@ -19,46 +19,50 @@ type foo struct {
 	Logout            string
 }
 
-var uploadTemplate, _ = template.ParseFile("upload.html")
-var viewTemplate, _ = template.ParseFile("view.html")
+var uploadTemplate = template.Must(template.ParseFile("upload.html"))
+var viewTemplate = template.Must(template.ParseFile("view.html"))
 
-func upload(w http.ResponseWriter, r *http.Request) {
-	filename := r.URL.Path[len("/post/"):]
-	c := appengine.NewContext(r)
-	if !user.IsAdmin(c) {
-		l, err := user.LoginURL(c, "/post/"+filename)
-		if err != nil {
-			panic(err)
-		}
+func upload(prefix string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		filename := r.URL.Path[len(prefix):]
+		c := appengine.NewContext(r)
+		if !user.IsAdmin(c) {
+			l, err := user.LoginURL(c, "/post/"+filename)
+			if err != nil {
+				panic(err)
+			}
 
-		http.Redirect(w, r, l, 302)
-		return
-	}
-	k := datastore.NewKey(c, "string", filename, 0, nil)
-	if r.Method == "GET" {
-		s := new(Page)
-		datastore.Get(c, k, s)
-		l, err := user.LogoutURL(c,"/")
-		if err != nil {
-			panic(err)
+			http.Redirect(w, r, l, 302)
+			return
 		}
-		uploadTemplate.Execute(w, foo{filename, s.Content, l})
-		return
+		k := datastore.NewKey(c, "string", filename, 0, nil)
+		if r.Method == "GET" {
+			s := new(Page)
+			datastore.Get(c, k, s)
+			l, err := user.LogoutURL(c, "/")
+			if err != nil {
+				panic(err)
+			}
+			uploadTemplate.Execute(w, foo{filename, s.Content, l})
+			return
+		}
+		content := r.FormValue("content")
+		output := html.EscapeString(content)
+		datastore.Put(c, k, &Page{output})
+		http.Redirect(w, r, "/view/"+filename, 302)
 	}
-	content := r.FormValue("content")
-	output := html.EscapeString(content)
-	datastore.Put(c, k, &Page{output})
-	http.Redirect(w, r, "/view/"+filename, 302)
 }
 
-func view(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	p := r.URL.Path[len("/view/"):]
-	s := new(Page)
-	k := datastore.NewKey(c, "string", p, 0, nil)
-	datastore.Get(c, k, s)
-	output := string(blackfriday.MarkdownCommon([]byte(s.Content)))
-	viewTemplate.Execute(w, foo{p, output,""})
+func view(prefix string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := appengine.NewContext(r)
+		p := r.URL.Path[len(prefix):]
+		s := new(Page)
+		k := datastore.NewKey(c, "string", p, 0, nil)
+		datastore.Get(c, k, s)
+		output := string(blackfriday.MarkdownCommon([]byte(s.Content)))
+		viewTemplate.Execute(w, foo{p, output, ""})
+	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -69,8 +73,15 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type route func(string) http.HandlerFunc
+
+func handle(u string, p route) {
+	http.Handle(u, p(u))
+}
+
 func init() {
-	http.HandleFunc("/post/", upload)
-	http.HandleFunc("/view/", view)
+	handle("/view/", view)
+	handle("/post/", upload)
 	http.HandleFunc("/", index)
 }
+// indent
